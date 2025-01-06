21dd80935ca57183c2b6b27341195e407767007d
alonso trina <alonsotrina22@gmail.com>
2025-01-06 08:53:07 -0300

    subiendo modificaciones front

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,12 +1,46 @@
 // Actions taken when clicking the 'input-area'
-const inputPin = document.getElementById('inputPin');
-const confirmButton = document.getElementById('confirmButton');
+const inputs = document.querySelectorAll(".otp-field input");
 
-// Escuchar el evento 'input' en el campo de texto
-inputPin.addEventListener('input', () => {
-  if (inputPin.value.trim() !== '') {
-    confirmButton.classList.remove('disabled');
-  } else {
-    confirmButton.classList.add('disabled');
-  }
+inputs.forEach((input, index) => {
+  input.dataset.index = index;
+  input.addEventListener("paste", handlePaste);
+  input.addEventListener("keyup", autoSubmit);
 });
+
+function handlePaste(e) {
+  const data = e.clipboardData.getData("text");
+  const value = data.split("");
+  if (value.length == inputs.length) {
+    inputs.forEach((input, index) => {
+      input.value = value[index];
+    });
+    submit();
+  }
+}
+function autoSubmit(e) {
+  const input = e.target;
+  let value = input.value.trim();
+  input.value = "";
+  input.value = value ? value[0] : "";
+  let fieldIndex = input.dataset.index;
+  if (value.length > 0 && fieldIndex < inputs.length - 1) {
+    input.nextElementSibling.focus();
+  }
+  if (e.key == "Backspace" && fieldIndex > 0) {
+    input.previousElementSibling.focus();
+  }
+  if (fieldIndex == inputs.length - 1) {
+    submit();
+  }
+}
+
+function submit() {
+  var otp = "";
+  inputs.forEach((input) => {
+    otp += input.value;
+    input.disabled = true;
+    input.classList.add("disabled");
+  });
+  console.log(otp);
+  window.location.href = 'loading.html';
+}
